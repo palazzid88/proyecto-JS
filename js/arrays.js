89437be187dc23e2c3338eda22e56e2a47d1b909
palazzid88 <palazzid88@gmail.com>
2022-09-02 14:24:30 -0300

    aplicando arrays y funciones de orden superior

--- js/arrays.js
+++ js/arrays.js
@@ -1,20 +1,26 @@
 // variables:
 
+// Arrays :
 const productos = [];
 const carrito = [];
 const totalPrecio = [];
+
+// Variables de Login
 let user = "a";
 let pass = "1";
 
+// Variables de proceso:
 let productoFiltrado;
 let productoElegido;
 let productoCarrito;
 let precioProducto;
 let kilogramo;
 let totalPrecioKg = 0;
+let userIngresado;
+let passIngresado;
 
 
-
+// Constructor de objetos
 class producto {
     constructor (id, nombre, precio, stock, unidad, estacion, tipo) { 
     this.id = id;
@@ -27,6 +33,7 @@ class producto {
 }
 } 
 
+// Objetos construidos:
 let banana = new producto (1000, "banana", 180, "1000", "kg", "anual", "fruta");
 let manzana = new producto (1000, "manzana", 190, "1000", "kg", "anual", "fruta");
 let naranja = new producto (1000, "naranja", 200, "1000", "kg", "anual", "fruta");
@@ -35,29 +42,37 @@ let kiwi = new producto (1000, "kiwi", 310, "1000", "kg", "anual", "fruta");
 let pera = new producto (1000, "pera", 160, "1000", "kg", "anual", "fruta");
 
 
+// Objetos pusheados al Array de productos
 productos.push (banana, manzana, naranja, frutilla, kiwi, pera);
 console.log(productos);
 
 
 
-// Login
+
+
+// Login - acceso
 
 ingreso ();
-if (validacion) {
+if (validacion) { //EL pedir producto se dá si la validación del user es True
     pedirProducto ();
     while (validacion) {
     productoIngresado ()
-    }
-    sumarCarrito ();
 }
+sumarCarrito ();
+}
+
+
+
+
 
 // funciones:
 
+// Funcion Login 
 function ingreso () {
         for (i=0; i<3; i++) {
         
-            let userIngresado = prompt("ingrese su nombre de usuario:");
-            let passIngresado = prompt("Ingrese su contraseña")
+            userIngresado = prompt("ingrese su nombre de usuario:");
+            passIngresado = prompt("Ingrese su contraseña")
         
             if (userIngresado == user && pass == passIngresado) {
                 console.log("Ingreso exitoso")
@@ -93,28 +108,39 @@ function ingreso () {
     }
 }
 
+// funcion Ingresar producto por el cliente
 function pedirProducto () {
-    productoElegido = prompt("Que producto desea comprar: \n Manzana \n banana \n pera \n frutilla \n kiwi \n naranja \n o presiones n para salir");
+    productoElegido = prompt("Que producto desea comprar: \n Manzana \n banana \n pera \n frutilla \n kiwi \n naranja \n o presione n para salir");
     console.log(productoElegido);
     if (productoElegido === "n") {
         validacion = false
+        console.log(`${userIngresado} Gracias por utilizar nuestra cartilla on line!`)
     }
     else {
         validacion = true
     }
 }
 
+// 
 function productoIngresado () {
     productoFiltrado = productos.find (item => item.nombre === productoElegido);
     console.log(productoFiltrado);
+
     kilogramo = prompt("ingrese cuantos kilogramos desea");
     totalPrecioKg = productoFiltrado.precio * kilogramo;
     console.log(totalPrecioKg);
-    alert(`El producto seleccionado es ${productoFiltrado.nombre} el precio por kg $${productoFiltrado.precio} usted a solicitado ${kilogramo} kg, el total es de $${totalPrecioKg}`);
-    carrito.push(productoFiltrado);
+
+    alert(`El producto seleccionado es ${productoFiltrado.nombre} 
+    el precio por kg $${productoFiltrado.precio} 
+    usted a solicitado ${kilogramo} 
+    kg, el total es de $${totalPrecioKg}`);
+
+    carrito.push(productoFiltrado); //Array carrito de compras
     console.log(`carrito`, carrito);
-    totalPrecio.push(totalPrecioKg);
+
+    totalPrecio.push(totalPrecioKg); //Array subtotal de precios
     console.log(totalPrecio);
+
     pedirProducto ();
 }
 
